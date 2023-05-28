17ab2a579fcf0276a5e42d7fca2367dce70ff1cf
drmcbride12 <130199368+drmcbride12@users.noreply.github.com>
2023-05-28 19:16:40 -0400

    Update tic_tac_toe.py
    Added wip gameplay features but not functional yet

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -16,9 +16,45 @@ def player_move(row, col, player):
         print('That space is already taken. Please choose another space.')
 
 
+def check_win(player):
+    # Check rows
+    for row in board:
+        if row == [player, player, player]:
+            return True
+    # Check columns
+    for i in range(3):
+        if board[0][i] == player and board[1][i] == player and board[2][i] == player:
+            return True
+    # Check diagonals
+    if board[0][0] == player and board[1][1] == player and board[2][2] == player:
+        return True
+    if board[0][2] == player and board[1][1] == player and board[2][0] == player:
+        return True
+    return False
+
+
+def ai_move():
+    import random
+    row = random.randint(0, 2)
+    col = random.randint(0, 2)
+    while board[row][col] != '-':
+        row = random.randint(0, 2)
+        col = random.randint(0, 2)
+    board[row][col] = 'O'
+
+
+def check_tie():
+    for row in board:
+        if '-' in row:
+            return False
+    return True
+
+
 # Test the functions
 print_board()
 player_move(0, 0, 'X')
+ai_move()
 print_board()
-player_move(1, 1, 'O')
-print_board()
+print(check_win('X'))
+print(check_win('O'))
+print(check_tie())
